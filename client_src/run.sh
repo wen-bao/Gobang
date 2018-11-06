8e32009ad4b2a312d9705113f4ce8727a9aef4ef
wen-bao <15579710692@163.com>
2018-11-06 22:52:37 +0800

    modify client

--- client_src/run.sh
+++ client_src/run.sh
@@ -1,9 +1,8 @@
 cd src
-pwd
-echo "building =====>"
-$(javac net/wenbaobao/*.java -d ../bin/)
-$(javac ai/*.java -d ../bin/)
-$(javac socket/*.java -d ../bin/)
-echo "running ====>"
-cd ../bin
-$(java net.wenbaobao.FiveChess)
+javac net/wenbaobao/*.java -d ../bin/
+javac ai/*.java -d ../bin/
+javac socket/*.java -d ../bin/
+cd ..
+jar -cvfm FiveChess.jar MANIFEST.MF -C bin/ .
+rm -rf bin/ai bin/socket bin/net
+
