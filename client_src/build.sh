48b348a1a6fff6d1d2b34f8c534dee4b3b2b2493
wen-bao <20499536+wen-bao@users.noreply.github.com>
2018-11-07 11:29:07 +0800

    Update build.sh

--- client_src/build.sh
+++ client_src/build.sh
@@ -1,7 +1,7 @@
 cd src
-javac -encoding net/wenbaobao/*.java -d ../bin/
-javac -encoding ai/*.java -d ../bin/
-javac -encoding socket/*.java -d ../bin/
+javac -encoding utf-8 net/wenbaobao/*.java -d ../bin/
+javac -encoding utf-8 ai/*.java -d ../bin/
+javac -encoding utf-8 socket/*.java -d ../bin/
 cd ..
 jar -cvfm FiveChess.jar MANIFEST.MF -C bin/ .
 rm -rf bin/ai bin/socket bin/net
